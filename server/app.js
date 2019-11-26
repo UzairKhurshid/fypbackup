bba0cdedc29ce8b57a8c5e273c069c7a1d0c73b4
Ali Raza <alir2282@gmail.com>
2019-11-26 15:32:32 +0500

    Theme Setup

--- server/app.js
+++ server/app.js
@@ -1,11 +1,30 @@
 const express =  require('express')
+const hbs = require('hbs')
+const path = require('path')
+
 const app = express()
+
+// Define path for express config
+const publicDirectory = path.join(__dirname, '../public')
+const viewsDirectory = path.join(__dirname, '../views')
+const partialsDirectory = path.join(__dirname, '../views/shared')
+
 
 const port = process.env.PORT || 3000
 
+// Setup handlers and views
+
+app.set('views', viewsDirectory)
+app.set('view options', { layout: '../views/layouts/main' });
+app.set('view engine','hbs')
+
+hbs.registerPartials(partialsDirectory)
+app.use(express.static(publicDirectory))
 
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.render('dashboard/index',{
+        title:'Dashboard'
+    })
   })
    
   app.listen(port)

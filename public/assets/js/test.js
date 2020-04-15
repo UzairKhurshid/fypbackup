f27ff5e6a18ab1f0fba8c08a3decc381c3b744d6
alirazahere <alir2282@gmail.com>
2020-04-15 17:34:37 +0500

    ui changes profile,account,project index file done.

--- public/assets/js/test.js
+++
@@ -1,6 +0,0 @@
-//alert('this is coming from js file')
-
-// function selectElement(id, valueToSelect) {    
-//     let element = document.getElementById(id);
-//     element.value = valueToSelect;
-// }
